df1f46e545c839e94b33d45f75a98b999385f6f2
zchesny <z.chesny@gmail.com>
2021-03-07 21:51:20 -0600

    filled in index and show user methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,9 +4,13 @@ class UsersController < ApplicationController
     end 
 
     def index 
+        users = User.all 
+        render json: UserSerializer.new(users).to_serialized_json
     end 
 
     def show 
+        user = User.find(params[:id])
+        render json: UserSerializer.new(user).to_serialized_json
     end 
 
     def new 
